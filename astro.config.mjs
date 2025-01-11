c7d0b3c078b5c25507ca88f7b59561621c815924
knhnd <ken.h.0802@gmail.com>
2025-01-11 12:57:53 +0900

    add github workflow file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://knhnd',
+	base: '/openlab-web',
 	integrations: [
 		starlight({
 			title: '💻 OpenLab',
